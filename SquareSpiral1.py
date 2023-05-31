3187605ebd2be2f1f0370819a013a31c9a5ce3d7
Daria RISY <taorisit@gmail.com>
2023-06-01 00:46:03 +0300

    color add

--- SquareSpiral1.py
+++ SquareSpiral1.py
@@ -1,6 +1,7 @@
 #  square spiral 1 - рисование квадратной сприрали.
 import turtle
 t = turtle.Pen()
-for x in range(500):
+t.color("red")
+for x in range(100):
     t.forward(x)
-    t.left(65)
+    t.left(91)
