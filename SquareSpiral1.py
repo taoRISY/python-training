2ccfcb89c93ecd5ff3b0eb7370b5791a6b592318
Daria RISY <taorisit@gmail.com>
2023-06-01 13:10:02 +0300

    ask a user to enter a number

--- SquareSpiral1.py
+++ SquareSpiral1.py
@@ -2,7 +2,7 @@
 import turtle
 turtle.bgcolor("black")
 t = turtle.Pen()
-sides=8
+sides=eval(input("Введите число от 1 до 8: "))
 colors = ["salmon","saddle brown","PaleVioletRed","plum1", "sea green", "silver", "sienna", "peru"] # переменная со списком цветов
 for x in range(360):
     t.color(colors[x%sides]) # 0<=x<=100(range), x%sides(6) - результатом будет остаток от деления
