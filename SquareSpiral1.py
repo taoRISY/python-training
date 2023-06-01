8ad028ecd69e9191815fa2a2cb40ff73eb96375a
Daria RISY <taorisit@gmail.com>
2023-06-01 12:41:35 +0300

    bgcolor change

--- SquareSpiral1.py
+++ SquareSpiral1.py
@@ -1,8 +1,9 @@
 #  square spiral 1.1 - рисование 4цетной кадратной сприрали.
 import turtle
+turtle.bgcolor("black")
 t = turtle.Pen()
 colors = ["salmon","saddle brown","PaleVioletRed","plum1"] # переменная со списком цветов
 for x in range(100):
-    t.color(colors[x%4])
-    t.forward(x)
+    t.color(colors[x%4]) # 0<=x<=100(range), x%4 - результатом будет остаток от деления. За 100 итераций цикла мы "пройдем" каждый цвет 25 раз
+    t.circle(x)
     t.left(91)
