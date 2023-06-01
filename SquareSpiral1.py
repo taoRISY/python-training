4bfd422d6104a31238184fe1d75292b4c8986706
Daria RISY <taorisit@gmail.com>
2023-06-01 13:14:20 +0300

    adding one more t.left

--- SquareSpiral1.py
+++ SquareSpiral1.py
@@ -8,5 +8,6 @@ for x in range(360):
     t.color(colors[x%sides]) # 0<=x<=100(range), x%sides(6) - результатом будет остаток от деления
     t.forward(x*3/sides+x)
     t.left(360/sides+1)
+    t.left(90)
     t.width(x*sides/200) # толщина линии
 
