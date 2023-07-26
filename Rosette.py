7ee86e496f35fed1b3928bf5babe7ae6d5393f09
Daria RISY <taorisit@gmail.com>
2023-07-26 23:02:51 +0300

    add-for-cycle-to-rosette

--- Rosette.py
+++ Rosette.py
@@ -1,9 +1,5 @@
 import turtle
 t = turtle.Pen()
-t.circle(100) # the 1st circle - north
-t.left(90)  # turn 
-t.circle(100)  # the 2nd circle - east
-t.left(90)  # turn 
-t.circle(100)  # the 3rd circle - south
-t.left(90)  # turn 
-t.circle(100)  # the 4th circle - west
+for x in range(4):
+    t.circle(100)
+    t.left(90)
