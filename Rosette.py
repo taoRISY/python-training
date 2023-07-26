a6de7615531cc5e9f76b402eac222d0b69b46b2f
Daria RISY <taorisit@gmail.com>
2023-07-26 22:56:36 +0300

    add-resette

---
+++ Rosette.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Pen()
+t.circle(100) # the 1st circle - north
+t.left(90)  # turn 
+t.circle(100)  # the 2nd circle - east
+t.left(90)  # turn 
+t.circle(100)  # the 3rd circle - south
+t.left(90)  # turn 
+t.circle(100)  # the 4th circle - west
